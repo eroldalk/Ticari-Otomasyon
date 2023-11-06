dbd255f83f0c2a6fce54c75d27835b60f7ad034d
Erol Dalkılıç <eroldalg@gmail.com>
2023-11-06 23:15:51 +0300

    FrmReports page

---
+++ Ticaret_Otomasyon/Ticaret_Otomasyon/FrmRaporlar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ticaret_Otomasyon
+{
+    public partial class FrmRaporlar : Form
+    {
+        public FrmRaporlar()
+        {
+            InitializeComponent();
+        }
+
+        private void gridView2_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmRaporlar_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+            this.reportViewer2.RefreshReport();
+        }
+    }
+}
